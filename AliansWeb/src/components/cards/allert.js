240b9fdf3450415a0ce49de5a3639e532fa271d3
mouataz1 <moataz.hakkou@gmail.com>
2021-12-24 19:31:25 +0100

    allerts using directus

--- AliansWeb/src/components/cards/allert.js
+++ AliansWeb/src/components/cards/allert.js
@@ -15,7 +15,7 @@ function Allerts() {
     useEffect(() => {
         axios.get('http://localhost:8055/items/alerts')
             .then(res => {
-                setAlerts(res.data);
+                setAlerts({id: res.data.id, title: res.data.taskTitle, description: res.data.description});
                 console.log(res.data);
 
             }).catch(error => alert("eroor alert !!"));
@@ -24,33 +24,24 @@ function Allerts() {
     // const handleChange = e =>{
     //     setSearch(e.table.value)
     // }
-   // const filterAlerts = alerts.filter(alert => alert.name.toLowerCase().includes(search.toLowerCase()))
+    //const filterAlerts = alerts.filter(alert => alert.name.toLowerCase().includes(search.toLowerCase()))
 
     return(
         <div className="container   p-0" >
-            {/*<div className="row d-flex justify-content-center align-items-center ">*/}
-            {/*    <div className="">*/}
+            <div className="row d-flex justify-content-center align-items-center ">
+                <div className="">
 
-            {/*        <div className="card " style={{color: '#ffff', width: '100%', borderRadius: '5px', backgroundColor: '#554BC5', height: '195px'}}>*/}
-            {/*            <div className="card-body ">*/}
-
-            {/*                {filterAlerts.map(alert => {*/}
-            {/*                    return (*/}
-            {/*                        <AlertData*/}
-            {/*                            key={alerts.id}*/}
-            {/*                            title={alerts.taskTitle}*/}
-            {/*                            desc={alerts.description}*/}
-
-            {/*                        />*/}
-            {/*                    )*/}
-            {/*                })}*/}
-
-
-
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+                    <div className="card " style={{color: '#ffff', width: '100%', borderRadius: '5px', backgroundColor: '#554BC5', height: '195px'}}>
+                        <div className="card-body ">
+                                    <AlertData
+                                        key={alerts.id}
+                                        title={alerts.title}
+                                        desc={alerts.description}
+                                    />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
